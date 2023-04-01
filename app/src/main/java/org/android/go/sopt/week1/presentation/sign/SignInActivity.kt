d1a30993daeb80f92ecbcd773eed24147bcb1567
Kim hajeong <happykhj67@gmail.com>
2023-04-02 02:06:14 +0900

    [feat] 로그인 액티비티 구현

--- app/src/main/java/org/android/go/sopt/week1/presentation/sign/SignInActivity.kt
+++ app/src/main/java/org/android/go/sopt/week1/presentation/sign/SignInActivity.kt
@@ -1,36 +1,38 @@
-package org.android.go.sopt.week1.presentation.login
+package org.android.go.sopt.week1.presentation.sign
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 import org.android.go.sopt.R
-import org.android.go.sopt.databinding.ActivityLoginBinding
-import org.android.go.sopt.week1.data.UserInfo
-import org.android.go.sopt.week1.presentation.signUp.SignUpActivity
+import org.android.go.sopt.databinding.ActivitySignInBinding
+import org.android.go.sopt.week1.data.User
+import org.android.go.sopt.week1.presentation.profile.ProfileActivity
 import org.android.go.sopt.week1.utill.BindingActivity
 import org.android.go.sopt.week1.utill.getParcelable
 
-class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
-    private var id: String? = null
-    private var pw: String? = null
+    private var user: User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initLoginBtnClickListener()
         initSignUpBtnClickListener()
+        setSignUp()
     }
 
     private fun initLoginBtnClickListener() {
-        binding.btnMainLogin.setOnClickListener {
-            if (binding.etMainId.text.toString() == id && binding.etMainId.text.toString() == pw) {
-                Snackbar.make(binding.root, "로그인에 성공했습니다.", Snackbar.LENGTH_SHORT).show()
-                finish()
+        binding.btnSignInLogin.setOnClickListener {
+            if (isSuccessSignIn()) {
+                goToProfile()
             } else {
-                Snackbar.make(binding.root, "다시 확인해주세요.", Snackbar.LENGTH_SHORT).show()
+                Snackbar.make(
+                    binding.root,
+                    "로그인 실패했습니다.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
             }
         }
     }
@@ -39,17 +41,36 @@ class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_lo
         startActivity(Intent(this, SignUpActivity::class.java))
     }
 
-    private fun setSignUpResult() {
+    private fun goToProfile() {
+        startActivity(Intent(this, ProfileActivity::class.java))
+        finish()
+    }
+
+    private fun setSignUp() {
         resultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode != Activity.RESULT_OK) return@registerForActivityResult
-                val data = result.data ?: return@registerForActivityResult
-
-                val userInfo = data.getParcelable(USER_INFO, UserInfo::class.java)
+                if (result.resultCode == RESULT_OK) {
+                    if (result.data?.hasExtra("user") == true) {
+                        Snackbar.make(
+                            binding.root,
+                            "회원가입에 성공했습니다.",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                        user = result.data?.getParcelable(USER, User::class.java)
+                    }
+                    with(binding) {
+                        etSignInId.setText(user?.id)
+                        etSignInPw.setText(user?.pw)
+                    }
+                }
             }
     }
 
+    private fun isSuccessSignIn(): Boolean {
+        return binding.etSignInId.text.toString() == user?.id && binding.etSignInPw.text.toString() == user?.pw
+    }
+
     companion object {
-        const val USER_INFO = "userInfo"
+        const val USER = "user"
     }
 }
