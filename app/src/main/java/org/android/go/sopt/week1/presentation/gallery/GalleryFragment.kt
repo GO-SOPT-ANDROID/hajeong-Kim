360219e14054bb0be71366133e38947095f3393a
Kim hajeong <happykhj67@gmail.com>
2023-04-21 23:17:16 +0900

    [add] 갤러리 fragment 추가

--- app/src/main/java/org/android/go/sopt/week1/presentation/gallery/GalleryFragment.kt
+++ app/src/main/java/org/android/go/sopt/week1/presentation/gallery/GalleryFragment.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.View
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentGalleryBinding
-import org.android.go.sopt.week1.utill.BindingFragment
+import org.android.go.sopt.week1.utill.binding.BindingFragment
 
 class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragment_gallery) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        private const val TAG = "GalleryFragment"
+
+        fun newInstance(): GalleryFragment {
+            return GalleryFragment()
+        }
     }
 }
